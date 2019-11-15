78bebc8a061a24b40b64312534ee4d835170a718
anthonypiazza <piazza.anthony.n@gmail.com>
2019-11-15 11:32:09 -0600

    Completed Ring Buffer Exercise

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,3 +1,5 @@
+from math import remainder
+
 class RingBuffer:
   def __init__(self, capacity):
     self.capacity = capacity
@@ -5,7 +7,36 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    # print(item)
+    # print(f"Self Capacity: {self.capacity}, Current Size: {self.current}, Modulo: {remainder(self.current, 5)}")
+    if self.current >= self.capacity:
+      if self.current == 5:
+        del self.storage[0]
+        self.storage.insert(0, item)
+        self.current = 6
+      elif self.current == 6:
+        del self.storage[1]
+        self.storage.insert(1, item)
+        self.current = 7
+      elif self.current == 7:
+        del self.storage[2]
+        self.storage.insert(2, item)
+        self.current = 8
+      elif self.current == 8:
+        del self.storage[3]
+        self.storage.insert(3, item)
+        self.current = 9
+      elif self.current == 9:
+        del self.storage[4]
+        self.storage.insert(4, item)
+        self.current = 5
+    else:
+      self.current+=1
+      del self.storage[0]
+      self.storage.insert(4, item)
+    # print(self.current)
+    
 
   def get(self):
-    pass
+    # print([i for i in self.storage if i is not None])
+    return [i for i in self.storage if i is not None]
